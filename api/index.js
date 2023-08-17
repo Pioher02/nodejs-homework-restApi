325ba4ec6222092f22b90c807c6a58ecb337642e
Pioher02 <dpaola.hernandezr@gmail.com>
2023-08-16 20:10:50 -0500

    Signup and login

---
+++ api/index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const User = require("../schemas/user");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const secret = process.env.SECRET;
+
+//const signupCtrl = require("../controller/signup.controller");
+// const loginCtrl = require("../controller/login.controller");
+
+router.post("/signup", async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user) {
+    return res.status(409).json({
+      status: "error",
+      code: 409,
+      message: "Email is already in use",
+      data: "Conflict",
+    });
+  }
+  try {
+    const newUser = new User({ username, email });
+    newUser.setPassword(password);
+    await newUser.save();
+    res.status(201).json({
+      status: "success",
+      code: 201,
+      data: {
+        message: "Registration successful",
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user || !user.validPassword(password)) {
+    return res.status(400).json({
+      status: "error",
+      code: 400,
+      message: "Incorrect login or password",
+      data: "Bad request",
+    });
+  }
+
+  const payload = {
+    id: user.id,
+    username: user.username,
+  };
+
+  const token = jwt.sign(payload, secret, { expiresIn: "1h" });
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      token,
+    },
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,50 @@
-const app = require('./app')
+const express = require("express");
+const cors = require("cors");
+const connection = require("./db/connection");
+const routerApi = require ("./api");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+require("./config/config-passport");
+
+app.use("/api", routerApi);
+
+app.use((_, res) => {
+  res.status(404).json({
+    status: "error",
+    code: 404,
+    message: `Use api on routes: 
+    /api/registration - registration user {username, email, password}
+    /api/login - login {email, password}
+    /api/list - get message if user is authenticated`,
+    data: "Not found",
+  });
+});
+
+app.use((err, _, res) => {
+  console.log(err.stack);
+  res.status(500).json({
+    status: "fail",
+    code: 500,
+    message: err.message,
+    data: "Internal Server Error",
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+connection
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Database connection successful on port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Server not running.
+        Error message: ${err.message}`);
+  });
